9b4a62fbe712677d4cd72c9b8795cfa5a288586a
Dao Nguyen V <dao.nguyen@supremetech.vn>
2021-01-25 17:58:14 +0700

    add random texts

--- src/app.tsx
+++ src/app.tsx
@@ -1,18 +1,54 @@
-import React from 'react'
+import React, { Component, ReactPropTypes } from 'react'
 import ReactDom from 'react-dom'
 import { Helmet } from 'react-helmet'
 import { env } from './config'
 
-class App extends React.Component {
+class App extends Component {
+  timeout: NodeJS.Timeout
+  state: { textIdx: number }
+  textArray: string[]
+
+  constructor(props: ReactPropTypes) {
+    super(props)
+    this.state = { textIdx: 0 }
+    this.textArray = [
+      'eat',
+      'sleep',
+      'drink',
+      'snore',
+      'foo',
+      'buzz',
+      'whatever',
+    ]
+  }
+
+  componentDidMount() {
+    this.timeout = setInterval(() => {
+      const idx = this.state.textIdx
+      this.setState({ textIdx: idx + 1 })
+    }, 1500)
+  }
+
+  componentDidUnmount() {
+    clearInterval(this.timeout)
+  }
+
   render() {
+    const text = this.textArray[this.state.textIdx % this.textArray.length]
     return (
       <div className="app">
         <Helmet>
           <meta charSet="utf-8" />
-          <title>My Title</title>
+          <title>React SEO</title>
           <link rel="canonical" href={env.CANONICAL_URL} />
         </Helmet>
         <div>Hello world!</div>
+        <section>
+          <h1>I&apos;m a React developer</h1>
+          <p>
+            I like to <span>{text}</span>
+          </p>
+        </section>
       </div>
     )
   }
